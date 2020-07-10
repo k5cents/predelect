2053d6a04191365a7357a0356aba83245720a293
Kiernan Nicholls <kiernann@protonmail.com>
2020-07-10 10:28:54 -0400

    Fix file dates

--- code/run-scrape.R
+++ code/run-scrape.R
@@ -2,12 +2,13 @@
 pacman::p_load(readr, dplyr, tidyr, lubridate, stringr, here, fs, glue)
 pacman::p_load_gh("kiernann/predictr")
 setwd("/home/kiernan/Code/bet-2020/")
+Sys.sleep(60)
 
 # get all markets once
 all_markets <- open_markets()
 
 # note the run hour
-tnow <- date()
+tnow <- today() - days(1)
 
 # run all the scripts
 message(x <- paste("#", tnow, "------------------------------"))
