d474a19d5266af6929df277297dce13f724115ef
Kiernan Nicholls <kiernann@protonmail.com>
2020-07-10 10:25:05 -0400

    Use setwd and direct paths

--- code/run-scrape.R
+++ code/run-scrape.R
@@ -1,6 +1,7 @@
 # load packages
 pacman::p_load(readr, dplyr, tidyr, lubridate, stringr, here, fs, glue)
 pacman::p_load_gh("kiernann/predictr")
+setwd("/home/kiernan/Code/bet-2020/")
 
 # get all markets once
 all_markets <- open_markets()
@@ -10,7 +11,7 @@ tnow <- date()
 
 # run all the scripts
 message(x <- paste("#", tnow, "------------------------------"))
-source(here("code", "scrape-house.R"))
-source(here("code", "scrape-senate.R"))
-source(here("code", "scrape-potus.R"))
+source("code/scrape-house.R")
+source("code/scrape-senate.R")
+source("code/scrape-potus.R")
 message(rep("#", nchar(x)))
